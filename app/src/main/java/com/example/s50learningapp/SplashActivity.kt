b26f727c3c4e7d8ef887c339d9d4576d6b78f1b8
sixpcoder <alirounak999@gmail.com>
2023-06-24 00:44:37 +0530

    initial commit

---
+++ app/src/main/java/com/example/s50learningapp/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.s50learningapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+    }
+}
